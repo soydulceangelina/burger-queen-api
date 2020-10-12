63d8ac2603183c6c22546fa151fe196915fc3517
Dulce Mercado <soydulceangelina@gmail.com>
2020-10-12 16:59:34 -0500

    cors express

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const env = require('./env');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods',
+      'PUT,POST,PATCH,DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
 platesApi(app);
 
 app.listen(env.port, () => {
